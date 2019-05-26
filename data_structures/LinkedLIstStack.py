c5dfa91082a90d7f87034af7edd0913aed7bad65
michal <m.kozel92@gmail.com>
2019-05-26 14:25:49 +0800

    Added stack implemented using a Linked list

---
+++ data_structures/LinkedLIstStack.py
@@ -0,0 +1,53 @@
+from typing import Any
+
+
+class LinkedListStack(object):
+    """
+    A Stack implemented using a Linked list
+    """
+
+    class Node(object):
+        """"Node for a Linked list"""
+        def __init__(self, data):
+            self.next = None
+            self.data = data
+
+    def __init__(self):
+        self.first = None
+
+    def push(self, data: Any):
+        """
+        pushes data on the top of the stack
+        :param data: data to push
+        """
+        tmp = self.first
+        self.first = self.Node(data)
+        self.first.next = tmp
+
+    def peek(self) -> Any:
+        """
+        return data from top of the stack without removing the top node
+        :return: data from the top node
+        """
+        return self.first.data
+
+    def pop(self) -> Any:
+        """
+        removes top node and returns its data
+        :return: data from to top node
+        """
+        data = self.first.data
+        self.first = self.first.next
+        return data
+
+    def is_empty(self):
+        """
+        :return: True if the Stack is empty
+        """
+        return self.first is None
+
+    def print_stack(self):
+        node = self.first
+        while node:
+            print(node.data)
+            node = node.next
