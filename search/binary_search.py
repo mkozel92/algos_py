6fe7fe1ea96cfb37a8ac34af4f994b096e90f2c9
michal <m.kozel92@gmail.com>
2019-06-04 18:08:36 +0800

    added recursive binary search

--- search/binary_search.py
+++ search/binary_search.py
@@ -20,3 +20,26 @@ def binary_search(a: list, to_search: int) -> int:
         else:
             return mid
     return -1
+
+
+def recursive_binary_search(a_list: list, to_search: int, from_: int, to_: int) -> int:
+    """
+    recursive implementation of binary search
+    complexity O(log N)
+    :param a_list: list to search
+    :param to_search: element to search for
+    :param from_: index to search from
+    :param to_:  index to search to
+    :return: index of the element of -1
+    """
+
+    if from_ > to_:
+        return -1
+
+    mid = from_ + (to_ - from_) // 2
+    if a_list[mid] < to_search:
+        return recursive_binary_search(a_list, to_search, mid + 1, to_)
+    elif a_list[mid] > to_search:
+        return recursive_binary_search(a_list, to_search, from_, mid - 1)
+    else:
+        return mid
--- tests/tests_search/test_binary_search.py
+++ tests/tests_search/test_binary_search.py
@@ -1,6 +1,6 @@
 import unittest
 
-from search.binary_search import binary_search
+from search.binary_search import binary_search, recursive_binary_search
 
 
 class TestBinarySearch(unittest.TestCase):
@@ -22,3 +22,18 @@ class TestBinarySearch(unittest.TestCase):
         self.assertEqual(-1, binary_search(a_list, 100))
         self.assertEqual(-1, binary_search(a_list, -1))
         self.assertEqual(-1, binary_search(a_list, 150))
+
+    def test_empty_list_recursive(self):
+        self.assertEqual(-1, recursive_binary_search([], 5, 0, len([]) - 1))
+
+    def test_present_elements_recursive(self):
+        a_list = list(range(100))
+        self.assertEqual(10, recursive_binary_search(a_list, 10, 0, len(a_list) - 1))
+        self.assertEqual(0, recursive_binary_search(a_list, 0, 0, len(a_list) - 1))
+        self.assertEqual(99, recursive_binary_search(a_list, 99, 0, len(a_list) - 1))
+
+    def test_missing_elements_recursive(self):
+        a_list = list(range(100))
+        self.assertEqual(-1, recursive_binary_search(a_list, 100, 0, len(a_list) - 1))
+        self.assertEqual(-1, recursive_binary_search(a_list, -1, 0, len(a_list) - 1))
+        self.assertEqual(-1, recursive_binary_search(a_list, 150, 0, len(a_list) - 1))
