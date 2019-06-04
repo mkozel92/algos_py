7cbc28c82dee87d470419309269ed0445d5d5334
michal <m.kozel92@gmail.com>
2019-06-04 19:24:20 +0800

    added kth_element_using three way partitioning

--- search/kth_largest.py
+++ search/kth_largest.py
@@ -1,6 +1,8 @@
 from sort.knuth_shuffle import knuth_shuffle
 from sort.quick_sort import partition
-from typing import Any
+from typing import Any, Tuple
+
+from sort.sort_utils import swap
 
 
 def quick_find(a_list: list, k: int) -> Any:
@@ -31,6 +33,57 @@ def quick_find(a_list: list, k: int) -> Any:
     return a_list[k]
 
 
-a_list = list(range(100))
-for k in range(100):
-    quick_find(a_list, k)
+def three_way_partition(a_list: list, from_: int, to_: int) -> Tuple[int, int]:
+    """
+    tree way partition that split the array to the tree parts
+    lesser, equal and greater than the partitioning element
+    :param a_list: list to partition
+    :param from_: partition from here
+    :param to_: partition to here
+    :return: lt and gt separators
+    """
+    v = a_list[from_]
+    i = from_
+    lt = from_
+    gt = to_
+
+    while i <= gt:
+        if a_list[i] < v:
+            swap(a_list, i, lt)
+            i += 1
+            lt += 1
+        elif a_list[i] > v:
+            swap(a_list, i, gt)
+            gt -= 1
+        else:
+            i += 1
+
+    return lt, gt
+
+
+def quick_find_three(a_list: list, k: int) -> Any:
+    """
+    function to find kth largest element in a list.
+    quick find is based on quick sort partitioning and stops when we
+    partitioned the list on k-th element
+    complexity worst case O(N^2)
+               guaranteed by shuffle O(N log N)
+               in practice run ~N
+
+    :param a_list: a list to search in
+    :param k: k specifying what to search for
+    :return: k-th largest element
+    """
+    knuth_shuffle(a_list)
+    low = 0
+    hi = len(a_list) - 1
+
+    while low <= hi:
+        lt, gt = three_way_partition(a_list, low, hi)
+        if lt > k:
+            hi = lt - 1
+        elif gt < k:
+            low = gt + 1
+        else:
+            return a_list[lt]
+    return a_list[low]
--- tests/tests_search/test_kth_largest.py
+++ tests/tests_search/test_kth_largest.py
@@ -1,6 +1,6 @@
 import unittest
 
-from search.kth_largest import quick_find
+from search.kth_largest import quick_find, quick_find_three
 
 
 class TestKthLargest(unittest.TestCase):
@@ -9,3 +9,15 @@ class TestKthLargest(unittest.TestCase):
         a_list = list(range(100))
         for k in range(100):
             self.assertEqual(quick_find(a_list, k), k)
+
+    def test_kth_largest_three(self):
+        a_list = list(range(100))
+        for k in range(100):
+            self.assertEqual(quick_find_three(a_list, k), k)
+
+        a_list = [1, 2, 5, 4, 5, 5, 7, 8, 5]
+        self.assertEqual(quick_find_three(a_list, 3), 5)
+        self.assertEqual(quick_find_three(a_list, 4), 5)
+        self.assertEqual(quick_find_three(a_list, 5), 5)
+        self.assertEqual(quick_find_three(a_list, 6), 5)
+        self.assertEqual(quick_find_three(a_list, 7), 7)
