cd3b0fe97b2b6e6688c229230379b45198853946
michal <m.kozel92@gmail.com>
2019-06-06 10:56:04 +0800

    added basic graph representations

---
+++ graphs/adjacency_list_graph.py
@@ -0,0 +1,35 @@
+
+
+class ALGraph(object):
+    """graph represented by adjacency list"""
+
+    def __init__(self, num_v):
+        """
+        init graph by number of vertices
+        :param num_v:  num of vertices in the graph
+        """
+        self.v = num_v
+        self.data = [set()] * self.v
+
+    def add_edge(self, p: int, q: int):
+        """
+        add edge between vertices p and q
+        complexity O(1)
+        :param p: a vertex
+        :param q: a vertex
+        """
+        assert (p < self.v)
+        assert (q < self.v)
+
+        self.data[p].add(q)
+        self.data[q].add(p)
+
+    def adj(self, p):
+        """
+        return set of vertices adjacent to p
+        complexity O(1)
+        :param p: a vertex
+        :return: set of vertices adjacent to p
+        """
+        assert (p < self.v)
+        return self.data[p]
---
+++ graphs/adjacency_matrix_graph.py
@@ -0,0 +1,40 @@
+from typing import Iterable
+
+
+class AMGraph(object):
+    """graph represented by adjacency matrix"""
+
+    def __init__(self, num_v: int):
+        """
+        initialize graph by vertex size
+        :param num_v: num of vertices in the graph
+        """
+        self.v = num_v
+        self.graph = [[0] * v] * v
+
+    def add_edge(self, p: int, q: int):
+        """
+        add edge to the graph between given vertices
+        complexity O(1)
+        :param p: first vertex
+        :param q: second vertex
+        """
+        assert (p < self.v)
+        assert (q < self.v)
+
+        self.graph[p][q] = 1
+        self.graph[q][p] = 1
+
+    def adj(self, v) -> Iterable:
+        """
+        return set of vertices adjacent to given vertex
+        complexity O(V)
+        :param v: a vertex
+        :return: set of vertices adjacent to v
+        """
+        assert (v < self.v)
+        adjacent_verices = set()
+        for i in range(self.v):
+            if self.graph[v][i] == 1:
+                adjacent_verices.add(i)
+        return adjacent_verices
