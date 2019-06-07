cb119ba9c7eddbd3039d6ea8e9696646a586b020
michal <m.kozel92@gmail.com>
2019-06-07 18:54:49 +0800

    Added Digraph implementation

---
+++ graphs/adjacency_list_digraph.py
@@ -0,0 +1,35 @@
+from graphs.graph_interface import Digraph
+
+
+class ALDigraph(Digraph):
+    """graph represented by adjacency list"""
+
+    def __init__(self, num_v):
+        """
+        init graph by number of vertices
+        :param num_v:  num of vertices in the graph
+        """
+        self.v = num_v
+        self.data = [set() for _ in range(self.v)]
+
+    def add_edge(self, p: int, q: int):
+        """
+        add directed edge between vertices p and q
+        complexity O(1)
+        :param p: a vertex
+        :param q: a vertex
+        """
+        assert (p < self.v)
+        assert (q < self.v)
+
+        self.data[p].add(q)
+
+    def adj(self, p):
+        """
+        return set of vertices adjacent to p
+        complexity O(1)
+        :param p: a vertex
+        :return: set of vertices adjacent to p
+        """
+        assert (p < self.v)
+        return self.data[p]
--- graphs/graph_interface.py
+++ graphs/graph_interface.py
@@ -20,3 +20,24 @@ class Graph(ABC):
         :param v_2: second vertex
         """
         pass
+
+
+class Digraph(ABC):
+    """interface for directed graph"""
+
+    @abstractmethod
+    def adj(self, v: int) -> set:
+        """
+        return set of adjacent vertices to a given vertex
+        :param v: a vertex
+        """
+        pass
+
+    @abstractmethod
+    def add_edge(self, v_1: int, v_2: int):
+        """
+        add new edge connecting given vertices
+        :param v_1: first vertex
+        :param v_2: second vertex
+        """
+        pass
