df09903c817ec68c3a09e6ba4b2714a9d16fa8d8
michal <m.kozel92@gmail.com>
2019-07-09 12:33:34 +0800

    added iterative version of power set

--- recursion/power_set.py
+++ recursion/power_set.py
@@ -7,7 +7,7 @@ def power_set_recursive(a_list: list, index: int) -> list:
     :return: all possible subsets
     """
     if index == len(a_list) - 1:
-        return [{a_list[-1]}, {}]
+        return [{a_list[-1]}, set([])]
 
     sub_sets = []
     for sub_set in power_set_recursive(a_list, index + 1):
@@ -15,3 +15,36 @@ def power_set_recursive(a_list: list, index: int) -> list:
         sub_sets.append(sub_set)
 
     return sub_sets
+
+
+def convert_to_set(i: int, a_list: list):
+    """
+    use integer as a binary mask to select from given list
+    :param i: mask
+    :param a_list: a list
+    :return: set with selected list elements
+    """
+    result = set([])
+    index = 0
+    while i != 0 and index < len(a_list):
+        if 1 & i:
+            result.add(a_list[index])
+        index += 1
+        i >>= 1
+
+    return result
+
+
+def power_set(a_list: list):
+    """
+    iterative implementation of power set
+    complexity O(2^n)
+    :param a_list: a list to process
+    :return: power set
+    """
+    count = 2**len(a_list)
+    result = []
+    for i in range(count):
+        result.append(convert_to_set(i, a_list))
+
+    return result
--- tests/tests_recursion/test_power_set.py
+++ tests/tests_recursion/test_power_set.py
@@ -1,6 +1,6 @@
 import unittest
 
-from recursion.power_set import power_set_recursive
+from recursion.power_set import power_set_recursive, power_set
 
 
 class TestPowerSet(unittest.TestCase):
@@ -9,3 +9,10 @@ class TestPowerSet(unittest.TestCase):
         self.assertEqual(len(power_set_recursive([1, 2, 3], 0)), 8)
         self.assertEqual(len(power_set_recursive([1, 2, 3, 4], 0)), 16)
         self.assertEqual(len(power_set_recursive([1, 2, 3, 4, 5], 0)), 32)
+
+        self.assertEqual(len(power_set([1, 2, 3])), 8)
+        self.assertEqual(len(power_set([1, 2, 3, 4])), 16)
+        self.assertEqual(len(power_set([1, 2, 3, 4, 5])), 32)
+
+    def test_compare(self):
+        self.assertEqual(power_set([1, 2, 3, 4, 5]).sort(), power_set_recursive([1, 2, 3, 4, 5], 0).sort())
